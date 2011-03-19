2283552e96c9329fb6c7e0f5dae57d76a3f4667a
Tareque Hossain <tareque@codexn.com>
2011-03-19 02:34:14 -0400

    Added logic for pasring configurations for syscallmux and sandbox. Updated logic for handlers.

--- sandbox/sandbox.c
+++ sandbox/sandbox.c
@@ -8,22 +8,33 @@
 #include "../kern_mux.h"
 
 #define MODULE_NAME "sandbox"
+#define MAX_SYSCALL 337
+
+#define DIRECTIVE_MAX_LENGTH 100
+#define DIRECTIVE_NEXT_KERNEL "next_kernel"
+#define DIRECTIVE_ALLOWED_SYSCALLS "allowed_syscalls"
+
+#define SCAN_REGEX_LENGTH 100
 
 extern int register_kernel(char* kernel_name, kmux_kernel_syscall_handler syscall_handler, kmux_kernel_config_handler config_handler);
 extern int unregister_kernel(char* kernel_name);
 extern int get_kernel_index(char* kernel_name);
 
-#define MAX_SYSCALL 337
-
-int allowed_syscalls[MAX_SYSCALL];
+int allowed_syscalls[MAX_SYSCALL + 1];
 int gnext_kernel_index = KMUX_HOST_KERNEL_INDEX;
 
 int sandbox_syscall_handler(struct pt_regs *regs) {
-    printk("sandbox_syscall_handler: Syscall number: %lu executing on %d\n", regs->ax, get_cpu());
-    // Add logic to enforce syscall filtering system call
+    int syscall_number = (int)regs->ax;
 
-    // If syscall is allowed, return index for syscallmux
-    return gnext_kernel_index;
+    if (syscall_number < 0 || syscall_number > MAX_SYSCALL) {
+        printk("sandbox_syscall_handler: Encountered invalid syscall number: %lu\n", regs->ax);
+    }
+
+    if (allowed_syscalls[syscall_number]) {
+        return gnext_kernel_index;
+    } else {
+        return KMUX_SYSCALL_EXIT_INDEX;
+    }
 }
 
 static int set_syscall_allowed(int syscall) {
@@ -35,16 +46,73 @@ static int set_syscall_allowed(int syscall) {
     return SUCCESS;
 }
 
+// Config buffer contains either of the following structures:
+// next_kernel = next_kernel_name
+// allowed_syscalls = 45, 67, 23, 178
 int sandbox_config_handler(char *config_buffer) {
-    printk("Configuring kernel: %s\n", MODULE_NAME);
+    char directive[DIRECTIVE_MAX_LENGTH], kernel_name[MAX_KERNEL_NAME_LENGTH], scan_regex[SCAN_REGEX_LENGTH];
+    int syscall_number;
+
+    memset(directive, 0, DIRECTIVE_MAX_LENGTH);
+    memset(kernel_name, 0, MAX_KERNEL_NAME_LENGTH);
+
+    printk("sandbox_config_handler: Configuring kernel: %s\n", MODULE_NAME);
+
+    if (config_buffer == NULL) {
+        printk("sandbox_config_handler: Received invalid config buffer\n");
+        return -EINVAL;
+    }
+
+    if (strlen(config_buffer) == 0) {
+        printk("sandbox_config_handler: Received zero length config buffer\n");
+        return -EINVAL;
+    }
+
     // Process buffer
+    memset(scan_regex, 0, SCAN_REGEX_LENGTH);
+    sprintf(scan_regex, "%%%d[^ =]%%*[ =]", DIRECTIVE_MAX_LENGTH);
+
+    if (sscanf(config_buffer, scan_regex, directive) < 0) {
+        printk("sandbox_config_handler: Error reading directive from config buffer\n");
+        return -EINVAL;
+    }
+
+    if (strcmp(directive, DIRECTIVE_NEXT_KERNEL) == 0) {
+        memset(scan_regex, 0, SCAN_REGEX_LENGTH);
+        sprintf(scan_regex, "%%%d", MAX_KERNEL_NAME_LENGTH);
+
+        if (sscanf(config_buffer, scan_regex, kernel_name) < 0) {
+            printk("sandbox_config_handler: Error reading next kernel name config buffer\n");
+            return -EINVAL;
+        }
+    } else if (strcmp(directive, DIRECTIVE_ALLOWED_SYSCALLS) == 0) {
+        while (sscanf(config_buffer, "%d%*[ ,]", &syscall_number) > 0) {
+            if (syscall_number < 0 || syscall_number > MAX_SYSCALL) {
+                printk("syscallmux_config_handler: Invalid syscall number: %d specified. Skipping\n", syscall_number);
+                continue;
+            }
+
+            allowed_syscalls[syscall_number] = 1;
+            printk("syscallmux_config_handler: Sandbox set to allow syscall: %d\n", syscall_number);
+        }
+    } else {
+        printk("sandbox_config_handler: Invalid directive: %s\n", directive);
+        return -EINVAL;
+    }
+
     return SUCCESS;
 }
 
 /* Module initialization/ termination */
 static int __init sandbox_init(void) {
+    int index;
     printk("Installing module: %s\n", MODULE_NAME);
-	register_kernel(MODULE_NAME, &sandbox_syscall_handler, &sandbox_config_handler);
+
+    for (index = 0; index < MAX_SYSCALL + 1; index++) {
+        allowed_syscalls[index] = 0;
+    }
+
+    register_kernel(MODULE_NAME, &sandbox_syscall_handler, &sandbox_config_handler);
 	return 0;
 }
 
--- syscallmux/syscall_mux.c
+++ syscallmux/syscall_mux.c
@@ -7,33 +7,80 @@
 #include "../kern_mux.h"
 
 #define MODULE_NAME "syscallmux"
+#define MAX_SYSCALL 337
+
+#define SCAN_REGEX_LENGTH 64
 
 extern int register_kernel(char* kernel_name, kmux_kernel_syscall_handler syscall_handler, kmux_kernel_config_handler config_handler);
 extern int unregister_kernel(char* kernel_name);
 extern int get_kernel_index(char *kernel_name);
 
+int syscall_kernel_register[MAX_SYSCALL + 1];
+
 int syscallmux_syscall_handler(struct pt_regs *regs) {
-    int next_kernel_index;
+    int next_kernel_index, syscall_number = (int)regs->ax;
 
-    printk("syscallmux_syscall_handler: Syscall number: %lu executing on %d\n", regs->ax, get_cpu());
-    // TODO: Ideally syscallmux would know about all syscall handling kernel installed
-    // If kernel is found for handling syscall, return index for that kernel
-    // Otherwise skip the syscall by returning -1
+    if (syscall_number < 0 || syscall_number > MAX_SYSCALL) {
+        printk("syscallmux_syscall_handler: Encountered invalid syscall number: %lu\n", regs->ax);
+    }
 
-    // For now send back host index
-    next_kernel_index = get_kernel_index(KMUX_DEFAULT_KERNEL_NAME);
+    next_kernel_index = syscall_kernel_register[syscall_number];
+
     return next_kernel_index;
 }
 
+// Config buffer contains one or more of the following structure separated by comma
+// syscall_number handling_kernel_name
 int syscallmux_config_handler(char *config_buffer) {
-    printk("Configuring kernel: %s\n", MODULE_NAME);
+    int syscall_number, kernel_index;
+    char kernel_name[MAX_KERNEL_NAME_LENGTH], scan_regex[SCAN_REGEX_LENGTH];
+
+    memset(kernel_name, 0, MAX_KERNEL_NAME_LENGTH);
+
+    printk("syscallmux_config_handler: Configuring kernel: %s\n", MODULE_NAME);
     // Process buffer
+    if (config_buffer == NULL) {
+        printk("syscallmux_config_handler: Received invalid config buffer\n");
+        return -EINVAL;
+    }
+
+    if (strlen(config_buffer) == 0) {
+        printk("syscallmux_config_handler: Received zero length config buffer\n");
+        return -EINVAL;
+    }
+
+    memset(scan_regex, 0, SCAN_REGEX_LENGTH);
+    sprintf(scan_regex, "%%d %%%d[^, ]%%*[, ]", MAX_KERNEL_NAME_LENGTH);
+
+    while (sscanf(config_buffer, scan_regex, &syscall_number, kernel_name) > 0) {
+        if (syscall_number < 0 || syscall_number > MAX_SYSCALL) {
+            printk("syscallmux_config_handler: Invalid syscall number: %d specified for kernel: %s. Skipping\n", syscall_number, kernel_name);
+            continue;
+        }
+
+        kernel_index = get_kernel_index(kernel_name);
+        if (kernel_index < 0) {
+            printk("syscallmux_config_handler: Invalid/non existent kernel: %s specified for syscall number: %d. Skipping\n", kernel_name, syscall_number);
+            continue;
+        }
+
+        syscall_kernel_register[syscall_number] = kernel_index;
+        printk("syscallmux_config_handler: Kernel: %s added for syscall: %d\n", kernel_name, syscall_number);
+    }
+
     return SUCCESS;
 }
 
 /* Module initialization/ termination */
 static int __init syscallmux_init(void) {
+    int index;
     printk("Installing module: %s\n", MODULE_NAME);
+
+    // As default, set all syscalls to be handled by host
+    for (index = 0; index < MAX_SYSCALL + 1; index++) {
+        syscall_kernel_register[index] = KMUX_HOST_KERNEL_INDEX;
+    }
+
     register_kernel(MODULE_NAME, &syscallmux_syscall_handler, &syscallmux_config_handler);
     return 0;
 }
