7f47e65fedbcfbaafcfd7e7b8a6b96eea6e4dba1
Tareque Hossain <tareque@codexn.com>
2011-04-03 13:05:28 -0400

    Removed support for multiple path per process. Too high memory consumption.

--- filesys_filter/filesys_filter.c
+++ filesys_filter/filesys_filter.c
@@ -20,7 +20,7 @@
 #define DIRECTIVE_UNREGISTER_PATH "unregister_path"
 
 #define MAX_PATH_SUPPORT 512
-#define MAX_PATH_PER_PROCESS 10
+#define MAX_PATH_LENGTH MAX_KERNEL_CONFIG_BUFFER_LENGTH
 
 extern int register_kernel(char* kernel_name, kmux_kernel_syscall_handler syscall_handler, kmux_kernel_config_handler config_handler);
 extern int unregister_kernel(char* kernel_name);
@@ -28,8 +28,7 @@ extern int get_kernel_index(char* kernel_name);
 
 struct path_entry {
     int pid;
-    int count;
-    char path[MAX_PATH_PER_PROCESS][PATH_MAX];
+    char path[MAX_PATH_LENGTH];
 };
 
 typedef struct path_entry path_entry;
@@ -127,7 +126,7 @@ static int find_path_register_slot(int pid) {
     hash_tracker = 0;
     while(hash_tracker < MAX_PATH_SUPPORT) {
         // Return index on empty or match
-        if (path_register[index].pid == -1 || path_register[index].pid) {
+        if (path_register[index].pid == -1 || path_register[index].pid == pid) {
             return index;
         }
         index = (index + 1) % MAX_PATH_SUPPORT;
@@ -140,6 +139,11 @@ static int find_path_register_slot(int pid) {
 static path_entry *lookup_path_entry(int pid) {
     int index;
     index = find_path_register_slot(pid);
+
+    if (index < 0) {
+        return NULL;
+    }
+
     if (path_register[index].pid == -1) {
         // pid is not in register
         return NULL;
@@ -149,8 +153,39 @@ static path_entry *lookup_path_entry(int pid) {
     }
 }
 
+static int register_path(int pid, char *path) {
+    int index;
+
+    index = find_path_register_slot(pid);
+    if (index < 0) {
+        return index;
+    }
+
+    if (path_register[index].pid == -1) {
+        path_register[index].pid = pid;
+        strcpy(path_register[index].path, path);
+        return SUCCESS;
+    } else {
+        return -EEXIST;
+    }
+}
+
+static int unregister_path(int pid) {
+    path_entry *path_info;
+
+    path_info = lookup_path_entry(pid);
+    if (!path_info) {
+        return -EINVAL;
+    }
+
+    path_info->pid = -1;
+    memset(path_info->path, 0, MAX_PATH_LENGTH);
+
+    return SUCCESS;
+}
+
 int filesys_filter_syscall_handler(struct pt_regs *regs) {
-    int index, ret_val = -EACCES, syscall_number = regs->ax;
+    int ret_val = -EACCES, syscall_number = regs->ax;
 
     if(syscall_number == 5 || syscall_number == 8) {
         char *normalized_path, *copied_file_path, *file_path, *matched_path;
@@ -187,15 +222,11 @@ int filesys_filter_syscall_handler(struct pt_regs *regs) {
 
         if (ret_val >= 0) {
             path_info = lookup_path_entry(current->pid);
-            if (path_info && path_info->count) {
-                for (index = 0; index < path_info->count; index++) {
-                    if (path_info->path[index][0]) {
-                        matched_path = strstr(normalized_path, path_info->path[index]);
-                        // NOTE: If normalized path doesn't start with restricted path, return error
-                        if (matched_path == normalized_path) {
-                            ret_val = gnext_kernel_index;
-                        }
-                    }
+            if (path_info) {
+                matched_path = strstr(normalized_path, path_info->path);
+                // NOTE: If normalized path doesn't start with restricted path, return error
+                if (matched_path == normalized_path) {
+                    ret_val = gnext_kernel_index;
                 }
             }
         }
@@ -207,20 +238,12 @@ int filesys_filter_syscall_handler(struct pt_regs *regs) {
     return ret_val;
 }
 
-int register_path(int pid, char* path) {
-    return SUCCESS;
-}
-
-int unregister_path(int pid) {
-    return SUCCESS;
-}
-
 // Config buffer contains either of the following structures:
 // (next_kernel = linux), (register_path = 3020, /tmp/firefox), (unregister_path = 3020)
 int filesys_filter_config_handler(char *config_buffer) {
     int pid, kernel_index;
-    char directive[DIRECTIVE_MAX_LENGTH], kernel_name[MAX_KERNEL_NAME_LENGTH], path[PATH_MAX], safe_config_buffer[MAX_KERNEL_CONFIG_BUFFER_LENGTH];
-    char *kernel_configs, *kernel_configs_tracker, *config_tokens, *config_tokens_tracker, *allowed_path, *allowed_path_tracker, *trimmed_config, *trimmed_config_token, *trimmed_path;
+    char directive[DIRECTIVE_MAX_LENGTH], kernel_name[MAX_KERNEL_NAME_LENGTH], safe_config_buffer[MAX_KERNEL_CONFIG_BUFFER_LENGTH];
+    char *kernel_configs, *kernel_configs_tracker, *config_tokens, *config_tokens_tracker, *allowed_path_tokens, *allowed_path_tokens_tracker, *trimmed_config, *trimmed_config_token, *trimmed_allowed_path_token;
 
     memset(directive, 0, DIRECTIVE_MAX_LENGTH);
     memset(kernel_name, 0, MAX_KERNEL_NAME_LENGTH);
@@ -277,12 +300,43 @@ int filesys_filter_config_handler(char *config_buffer) {
                                     gnext_kernel_index = kernel_index;
                                 }
                             } else if (strcmp(directive, DIRECTIVE_REGISTER_PATH) == 0) {
-                                // TODO: Extract process ID and path
-                                register_path(pid, path);
+                                allowed_path_tokens = strtok_r(trimmed_config_token, ",", &allowed_path_tokens_tracker);
+                                if (allowed_path_tokens) {
+                                    trimmed_allowed_path_token = trim(allowed_path_tokens, ' ');
+                                    if (strlen(trimmed_allowed_path_token)) {
+                                        pid = atoi(trimmed_allowed_path_token);
+                                        if (pid) {
+                                            allowed_path_tokens = strtok_r(NULL, ",", &allowed_path_tokens_tracker);
+                                            if (allowed_path_tokens) {
+                                                trimmed_allowed_path_token = trim(allowed_path_tokens, ' ');
+                                                if (strlen(trimmed_allowed_path_token)) {
+                                                    if (register_path(pid, trimmed_allowed_path_token) < 0) {
+                                                        printk("filesys_filter_config_handler: Could not register path %s for pid %d\n", trimmed_allowed_path_token, pid);
+                                                    } else {
+                                                        printk("filesys_filter_config_handler: Registered path %s for pid %d\n", trimmed_allowed_path_token, pid);
+                                                    }
+                                                } else {
+                                                    printk("filesys_filter_config_handler: Found empty path in allowed path parameters. Skipping\n");
+                                                }
+                                            } else {
+                                                printk("filesys_filter_config_handler: Could not read path from allowed path parameters. Skipping\n");
+                                            }
+                                        } else {
+                                            printk("filesys_filter_config_handler: Found invalid PID in allowed path parameters. Skipping\n");
+                                        }
+                                    } else {
+                                        printk("filesys_filter_config_handler: Found empty PID in allowed path parameters. Skipping\n");
+                                    }
+                                } else {
+                                    printk("filesys_filter_config_handler: Could not read PID from allowed path parameters. Skipping\n");
+                                }
                             } else if (strcmp(directive, DIRECTIVE_UNREGISTER_PATH) == 0) {
-                                // TODO: Extract process ID
                                 pid = atoi(trimmed_config_token);
-                                unregister_path(pid);
+                                if (unregister_path(pid) < 0) {
+                                    printk("filesys_filter_config_handler: Could not unregister paths for pid %d. Already unregistered?\n", pid);
+                                } else {
+                                    printk("filesys_filter_config_handler: Unregistered paths for pid %d\n", pid);
+                                }
                             } else {
                                 printk("filesys_filter_config_handler: Invalid directive in configuration piece. Skipping\n");
                             }
@@ -310,16 +364,12 @@ int filesys_filter_config_handler(char *config_buffer) {
 
 /* Module initialization/ termination */
 static int __init filesys_filter_init(void) {
-    int index, subindex;
+    int index;
     printk("Installing module: %s\n", MODULE_NAME);
 
     for (index = 0; index < MAX_PATH_SUPPORT; index++) {
         path_register[index].pid = -1;
-        path_register[index].count = 0;
-
-        for (subindex = 0; subindex < MAX_PATH_PER_PROCESS; subindex++) {
-            memset(path_register[index].path[subindex], 0, PATH_MAX);
-        }
+        memset(path_register[index].path, 0, MAX_PATH_LENGTH);
     }
 
     register_kernel(MODULE_NAME, &filesys_filter_syscall_handler, &filesys_filter_config_handler);
